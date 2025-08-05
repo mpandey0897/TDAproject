ac28fd31f814373e98485f69ff14a43c1152c20a
ivanhejny <ivan.hejny@gmail.com>
2025-08-05 17:10:57 +0200

    first persistency diagrams

--- first_ideas.py
+++ first_ideas.py
@@ -2,10 +2,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # This import registers the 3D projection
-
-print('hej')
-# get working directory
 import os
+import gudhi
+import math
+from ripser import ripser
+from persim import plot_diagrams
+from persim import PersistenceImager
+from gudhi.representations import Landscape as Landscape
+from gudhi.representations import PersistenceImage as PersImage
+from gudhi.subsampling import pick_n_random_points as pick_random
+
+
+
 # print current working directory
 print(os.getcwd())
 
@@ -18,15 +26,18 @@ print(bunny.head())
 #size of bunny
 print('bunny size:', bunny.shape)
 #pick random m indices from 0 to 4000
-idx = np.random.choice(bunny.shape[0], size=4000, replace=False)
+idx = np.random.choice(bunny.shape[0], size=40, replace=False)
 print('idx:',idx)
 
 
 print(bunny.iloc[idx])
 bunny_sub = bunny.iloc[idx]
-print(bunny_sub.head())
+print('bunny_sub:\n',bunny_sub.head())
+# Convert DataFrame to float NumPy array for gudhi
+bunny_points = bunny[['x', 'y', 'z']].astype(float).to_numpy()
+print('bunny_points:\n',bunny_points)
 
-
+'''
 # Create a 3D scatter plot
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111, projection='3d')
@@ -41,5 +52,10 @@ ax.set_zlabel('Z')
 ax.set_title('3D Scatter Plot of Bunny Data')
 
 plt.show()
+'''
 
-
+ac = gudhi.AlphaComplex(points = bunny_points)
+acx = ac.create_simplex_tree()
+diag = acx.persistence()
+gudhi.plot_persistence_diagram(diag)
+plt.show()
