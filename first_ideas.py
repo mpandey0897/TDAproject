5789a9e880734434578de3f105e4d7f09f70292b
ivanhejny <ivan.hejny@gmail.com>
2025-08-05 16:43:54 +0200

    found an error in original code file spaghetti

--- first_ideas.py
+++ first_ideas.py
@@ -15,11 +15,25 @@ bunny = pd.read_csv(os.getcwd() + "/data/bunny.txt")
 
 # Display the first few rows
 print(bunny.head())
+#size of bunny
+print('bunny size:', bunny.shape)
+#pick random m indices from 0 to 4000
+idx = np.random.choice(bunny.shape[0], size=4000, replace=False)
+print('idx:',idx)
+
+
+print(bunny.iloc[idx])
+bunny_sub = bunny.iloc[idx]
+print(bunny_sub.head())
+
 
 # Create a 3D scatter plot
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(bunny['x'], bunny['y'], bunny['z'], s=1, c='blue', alpha=0.6)
+
+
+#ax.scatter(bunny['x'], bunny['y'], bunny['z'], s=1, c='blue', alpha=0.6)
+ax.scatter(bunny_sub['x'], bunny_sub['y'], bunny_sub['z'], s=1, c='blue', alpha=0.6)
 
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
@@ -27,3 +41,5 @@ ax.set_zlabel('Z')
 ax.set_title('3D Scatter Plot of Bunny Data')
 
 plt.show()
+
+
