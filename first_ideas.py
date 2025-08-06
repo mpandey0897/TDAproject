60196bb7c604215b9f7a8ec1780334480614c084
ivanhejny <ivan.hejny@gmail.com>
2025-08-06 23:18:26 +0200

    some more persistency and barcodes for bunny point cloud, signal 2-cycle void clearly visible for 400 points

--- first_ideas.py
+++ first_ideas.py
@@ -12,7 +12,7 @@ from gudhi.representations import Landscape as Landscape
 from gudhi.representations import PersistenceImage as PersImage
 from gudhi.subsampling import pick_n_random_points as pick_random
 
-
+plt.rcParams['savefig.dpi'] = 300  # High-quality by default
 
 # print current working directory
 print(os.getcwd())
@@ -25,19 +25,25 @@ bunny = pd.read_csv(os.getcwd() + "/data/bunny.txt")
 print(bunny.head())
 #size of bunny
 print('bunny size:', bunny.shape)
-#pick random m indices from 0 to 4000
-idx = np.random.choice(bunny.shape[0], size=40, replace=False)
-print('idx:',idx)
 
+#pick random n indices
+np.random.seed(42)
+idx = np.random.choice(bunny.shape[0], size=400, replace=False)
+print('idx:',idx, 'len(idx):', len(idx))
 
 print(bunny.iloc[idx])
 bunny_sub = bunny.iloc[idx]
 print('bunny_sub:\n',bunny_sub.head())
+#bunny_sub = bunny_sub[['x', 'y', 'z']].astype(float).to_numpy()
+#print('bunny_sub:\n',bunny_sub)
+
+
 # Convert DataFrame to float NumPy array for gudhi
 bunny_points = bunny[['x', 'y', 'z']].astype(float).to_numpy()
-print('bunny_points:\n',bunny_points)
+print('bunny_points:\n', bunny_points)
 
-'''
+
+#'''
 # Create a 3D scatter plot
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111, projection='3d')
@@ -49,13 +55,44 @@ ax.scatter(bunny_sub['x'], bunny_sub['y'], bunny_sub['z'], s=1, c='blue', alpha=
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
-ax.set_title('3D Scatter Plot of Bunny Data')
+ax.set_title(f'3D Cloud of Bunny Data n={len(idx)}')
 
 plt.show()
-'''
+#'''
 
-ac = gudhi.AlphaComplex(points = bunny_points)
+
+
+ac = gudhi.AlphaComplex(points = bunny_points[idx]) #bunny_points
 acx = ac.create_simplex_tree()
 diag = acx.persistence()
+
+n = len(idx)
+
+# 2. Persistence Diagram
 gudhi.plot_persistence_diagram(diag)
+plt.title(f'Persistence Diagram of Bunny n={n}')
+plt.savefig(f"Bunny_{n}_persistence_diagram.png", dpi=300, bbox_inches='tight')
 plt.show()
+
+# 3. Barcode Diagram (all dimensions)
+gudhi.plot_persistence_barcode(diag)
+plt.title(f'Barcode (Persistent Betti Numbers) of Bunny n={n}')
+plt.savefig(f"Bunny_{n}_barcode.png", dpi=300, bbox_inches='tight')
+plt.show()
+
+# Filter persistence pairs to only dimension 2 (H2: voids), and finite intervals
+diag_H2 = [
+    (dim, pair) for dim, pair in diag
+    if dim == 2 and not math.isinf(pair[1]) and pair[1] > pair[0]
+]
+
+print(f"Number of finite H2 intervals: {len(diag_H2)}")
+
+# Plot barcodes for H2
+if diag_H2:
+    gudhi.plot_persistence_barcode(diag_H2)
+    plt.title(f'Barcode of H2 (2-cycles) for Bunny n={len(idx)}')
+    plt.savefig(f"Bunny_{n}_barcodeH2.png", dpi=300, bbox_inches='tight')
+    plt.show()
+else:
+    print("No finite H2 intervals to plot!")
